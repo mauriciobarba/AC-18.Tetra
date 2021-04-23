e1bf244405f73762b742bce73ca8b0aa653f14e1
Mauricio Barba da Costa <barba@mit.edu>
2021-04-23 18:19:38 -0400

    fixed get primes function

--- mautetra.py
+++ mautetra.py
@@ -104,16 +104,16 @@ def one_combination_matrix(n):
 
 
 def get_prime_factors(number):
-  prime_factors = []
+  prime_factors = set({})
   while number % 2 == 0:
-    prime_factors.append(2)
+    prime_factors.add(2)
     number = number / 2
   for i in range(3, int(math.sqrt(number)) + 1, 2):
     while number % i == 0:
-      prime_factors.append(int(i))
-      number = number / i
+      prime_factors.add(i)
+      number = number // i
   if number > 2:
-    prime_factors.append(int(number)) 
+    prime_factors.add(int(number))
 
   return prime_factors
 
